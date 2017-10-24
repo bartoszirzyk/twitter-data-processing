435cb635faeea24346c6dedb725764fe24725edb
Bartosz Irzyk <bartek9945@gmail.com>
2017-10-24 21:36:25 +0200

    PoC urls filtering

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,17 @@
 
+import re
+import urllib2
+
+def findRedirection(url):
+	req = urllib2.Request(url= url)
+	resp = urllib2.urlopen(req, timeout=3)
+	return resp.geturl()
+
+def filterURLs(tweet):
+	return  [findRedirection(x) for x in re.findall(r'(https?://[^\s]+)', tweet)] 
+
+
+output = []
+test = "https://www.google.com dsadsanfajsdnfdlasnf http://www.wp.pl"
+result = filterURLs(test)
+print(result)
