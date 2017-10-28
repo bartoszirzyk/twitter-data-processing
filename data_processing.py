55ca765023a175dcdb0847b60243543633128154
Bartosz Irzyk <bartek9945@gmail.com>
2017-10-28 20:43:02 +0200

    Add new dict field with transformed urls

--- data_processing.py
+++ data_processing.py
@@ -1,22 +1,30 @@
 
 # encoding=utf8
-import re
 import csv
 import json
-
+from urllib.parse import urlparse
+from urllib.parse import urlsplit
 
 def transformTweet(tweet):
-  tweet['urls'] = [transformURL(x) for x in json.loads(tweet['urls'])]
+  tweet['urls_transformed'] = [transformURL(x) for x in json.loads(tweet['urls'])]
   return tweet
 
+
 def transformURL(url):
+  out = {'url': '', 'domain': ''}
   if 'expanded_url' in url.keys():
-    return re.match('https?://[^\s\?\#]+', url['expanded_url']).group(0)
+    r = urlsplit(url['expanded_url'])
+    out['url'] = r.netloc + r.path
+  out['domain'] = r.netloc
+  return out
+
 
 with open('InputData/tweets2.csv', 'r', encoding="utf-8") as inputfile:
   reader = csv.DictReader(inputfile)
   with open('OutputData/out.csv', 'w') as outputfile:
-    writer = csv.DictWriter(outputfile, fieldnames=reader.fieldnames)
+    foo = reader.fieldnames
+    foo.append('urls_transformed')
+    writer = csv.DictWriter(outputfile, fieldnames=foo)
     writer.writeheader()
     for row in reader:
       writer.writerow(transformTweet(row))
