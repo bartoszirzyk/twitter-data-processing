c68d8e88a338a4cd8d56c68e17a531ba718fe6bc
Bartosz Irzyk <bartek9945@gmail.com>
2017-10-28 19:48:29 +0200

    extract transform tweet

--- data_processing.py
+++ data_processing.py
@@ -5,15 +5,18 @@ import csv
 import json
 
 
+def transformTweet(tweet):
+  tweet['urls'] = [transformURL(x) for x in json.loads(tweet['urls'])]
+  return tweet
+
 def transformURL(url):
-  if any(url):
+  if 'expanded_url' in url.keys():
     return re.match('https?://[^\s\?\#]+', url['expanded_url']).group(0)
 
 with open('InputData/tweets2.csv', 'r', encoding="utf-8") as inputfile:
   reader = csv.DictReader(inputfile)
   with open('OutputData/out.csv', 'w') as outputfile:
-    fieldnames = ['twid', 'urls']
-    writer = csv.DictWriter(outputfile, fieldnames=fieldnames)
+    writer = csv.DictWriter(outputfile, fieldnames=reader.fieldnames)
     writer.writeheader()
     for row in reader:
-      writer.writerow({'twid': row['twid'], 'urls': [transformURL(x) for x in json.loads(row['urls'])]})
+      writer.writerow(transformTweet(row))
